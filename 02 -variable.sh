d465f4feacc7c0be3c8dbd292bba1d83efdd621e
Aneshbunny <iamaneshathkuri@gmail.com>
2026-02-21 23:23:59 +0530

    one more script is written

---
+++ 02 -variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Anesh: Hai Naga, How are you?"
+echo "Naga: Hai Anesh,Iam good,what about you?"
+echo "Anesh: Iam over the moon, what are you doing?"
+echo "Naga: Iam learning Performance test,what about you?"
+echo "Anesh: Iam learning AWS and DevSecOps,Naga 
